ab56d39f4c187a6f44dbb596cc588e13f5f165e3
tushar2k03 <tushar7830970171@gmail.com>
2025-07-29 00:22:21 +0530

    Add marquee for sliding right to left comapany names

---
+++ src/Data/Data.tsx
@@ -0,0 +1,3 @@
+const companies = ["Google","Microsoft","Amazon","Figma","Netflix","Meta","Pinterest","Slack","Spotify","Oracle","Walmart"];
+
+export default companies
--- src/Pages/HomePage.tsx
+++ src/Pages/HomePage.tsx
@@ -1,4 +1,5 @@
 import Header from "../Header/Header";
+import Companies from "../LandingPage/Companies";
 import DreamJob from "../LandingPage/DreamJob";
 
 const HomePage=()=> {
@@ -6,6 +7,7 @@ const HomePage=()=> {
         <div className="min-h-[100vh] bg-mine-shaft-950 font-['poppins']]">
         <Header/>
         <DreamJob/>
+        <Companies/>
         </div>
     )
 }
